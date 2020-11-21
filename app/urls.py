8e8deebee64c2c42e26d8585f009f9fb0f1c10d5
truonggiang0302 <giangkoy0302@gmail.com>
2020-11-21 23:39:39 +0700

    update

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,12 @@ urlpatterns = [
     path('',index),
     path('all-product',viewAllProduct,name='all-product'),
     path('honda',Honda,name='honda'),
-    path('product/<pk>',viewProduct,name='product'),
+    path('yamaha',Yamaha,name='yamaha'),
+    path('ducati',Ducati,name='ducati'),
+    path('kawasaki',Kawasaki,name='kawasaki'),
+    path('bmw',BMW,name='bmw'),
+    path('suzuki',Suzuki,name='suzuki'),
+    path('ktm',KTM,name='ktm'),
     path('detail-product/<pk>',viewProductDetail,name='detail-product'),
     path('order-product/<pk>',orderProduct,name='order-product'),
 ]
--- app/views.py
+++ app/views.py
@@ -8,7 +8,20 @@ from datetime import datetime
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request,'index.html')
+
+priceList = [
+    {'id': 1, 'name': 'Dưới 100 triệu', 'max': 100},
+    {'id': 2, 'name': '100 triệu --> 300 triệu', 'min': 100, 'max': 300},
+    {'id': 3, 'name': '300 triệu --> 600 triệu', 'min': 300, 'max': 600},
+    {'id': 4, 'name': '600 triệu --> 900 triệu', 'min': 600, 'max': 900},
+    {'id': 5, 'name': 'Trên 900 triệu', 'min': 900},
+]
+ccList = [
+    {'id': 1, 'name': 'Dưới 300cc', 'max': 300},
+    {'id': 2, 'name': '300cc --> 600cc', 'min': 300, 'max': 600},
+    {'id': 3, 'name': 'Trên 1000cc', 'min': 1000},
+]
 
 
 def viewAllProduct(request):
@@ -20,39 +33,189 @@ def viewAllProduct(request):
     end = start + pageSize
     total = len(productList)
     num_page = math.ceil(total/pageSize)
+    # Tìm kiếm
+    name=request.GET.get('name','')
+    productList=Product.objects.filter(name__icontains=name)
+    categoryId = request.GET.get('categoryId')
+    categoryId = int(categoryId) if categoryId else None
+    if categoryId:
+        productList = productList.filter(category__id=categoryId)
+    categoryList = Category.objects.all()
+    priceId = request.GET.get('priceId')
+    priceId = int(priceId) if priceId else None
+    price = priceList[priceId-1] if priceId else {}
+    minPrice, maxPrice = price.get('min'), price.get('max')
+    if minPrice:
+        productList = productList.filter(price__gte=minPrice)
+
+    if maxPrice:
+        productList = productList.filter(price__lte=maxPrice)
+
+    ccId = request.GET.get('ccId')
+    ccId = int(ccId) if ccId else None
+    cc = ccList[ccId-1] if ccId else {}
+    minCC = cc.get('min')
+    maxCC = cc.get('max')
+    if minCC:
+        productList = productList.filter(cc__gte=minCC)
+    if maxCC:
+        productList = productList.filter(cc__lte=maxCC)
+    #Context
     context = {
         'productList': productList[start:end],
         'start': start,
         'end': end,
-        'total': total,
         'num_page': num_page,
-        'page': page
+        'page': page,
+        'name':name,
+        'priceList': priceList,
+        'ccList':ccList,
+        'categoryList': categoryList,
+        'productList': productList,
+        'categoryId': categoryId,
+        'priceId': priceId,
+        'ccId':ccId,
     }
     return render(request, 'view_all_product.html', context)
 
 def Honda(request):
-    product=Product.objects.filter(category__id=1)
-    context={'product':product}
-    return render(request,'motor/honda.html')
-
-def viewProduct(request, pk):
-    productList = Product.objects.filter(category__id=pk)
+    product = Product.objects.filter(category__id=1)
     page = request.GET.get('page', '')
     page = int(page) if page.isdigit() else 1
     pageSize = settings.PAGE_SIZE
     start = (page-1)*pageSize
     end = start + pageSize
-    total = len(productList)
+    total = len(product)
     num_page = math.ceil(total/pageSize)
     context = {
-        'productList': productList[start:end],
+        'product': product[start:end],
         'start': start,
         'end': end,
         'total': total,
         'num_page': num_page,
         'page': page
     }
-    return render(request, 'view_product.html', context)
+    return render(request, 'motor/honda.html', context)
+
+
+def Yamaha(request):
+    product = Product.objects.filter(category__id=2)
+    page = request.GET.get('page', '')
+    page = int(page) if page.isdigit() else 1
+    pageSize = settings.PAGE_SIZE
+    start = (page-1)*pageSize
+    end = start + pageSize
+    total = len(product)
+    num_page = math.ceil(total/pageSize)
+    context = {
+        'product': product[start:end],
+        'start': start,
+        'end': end,
+        'total': total,
+        'num_page': num_page,
+        'page': page
+    }
+    return render(request, 'motor/yamaha.html', context)
+
+
+def Ducati(request):
+    product = Product.objects.filter(category__id=3)
+    page = request.GET.get('page', '')
+    page = int(page) if page.isdigit() else 1
+    pageSize = settings.PAGE_SIZE
+    start = (page-1)*pageSize
+    end = start + pageSize
+    total = len(product)
+    num_page = math.ceil(total/pageSize)
+    context = {
+        'product': product[start:end],
+        'start': start,
+        'end': end,
+        'total': total,
+        'num_page': num_page,
+        'page': page
+    }
+    return render(request, 'motor/ducati.html', context)
+
+
+def Kawasaki(request):
+    product = Product.objects.filter(category__id=4)
+    page = request.GET.get('page', '')
+    page = int(page) if page.isdigit() else 1
+    pageSize = settings.PAGE_SIZE
+    start = (page-1)*pageSize
+    end = start + pageSize
+    total = len(product)
+    num_page = math.ceil(total/pageSize)
+    context = {
+        'product': product[start:end],
+        'start': start,
+        'end': end,
+        'total': total,
+        'num_page': num_page,
+        'page': page
+    }
+    return render(request, 'motor/kawasaki.html', context)
+
+
+def BMW(request):
+    product = Product.objects.filter(category__id=5)
+    page = request.GET.get('page', '')
+    page = int(page) if page.isdigit() else 1
+    pageSize = settings.PAGE_SIZE
+    start = (page-1)*pageSize
+    end = start + pageSize
+    total = len(product)
+    num_page = math.ceil(total/pageSize)
+    context = {
+        'product': product[start:end],
+        'start': start,
+        'end': end,
+        'total': total,
+        'num_page': num_page,
+        'page': page
+    }
+    return render(request, 'motor/bmw.html', context)
+
+
+def Suzuki(request):
+    product = Product.objects.filter(category__id=6)
+    page = request.GET.get('page', '')
+    page = int(page) if page.isdigit() else 1
+    pageSize = settings.PAGE_SIZE
+    start = (page-1)*pageSize
+    end = start + pageSize
+    total = len(product)
+    num_page = math.ceil(total/pageSize)
+    context = {
+        'product': product[start:end],
+        'start': start,
+        'end': end,
+        'total': total,
+        'num_page': num_page,
+        'page': page
+    }
+    return render(request, 'motor/suzuki.html', context)
+
+
+def KTM(request):
+    product = Product.objects.filter(category__id=7)
+    page = request.GET.get('page', '')
+    page = int(page) if page.isdigit() else 1
+    pageSize = settings.PAGE_SIZE
+    start = (page-1)*pageSize
+    end = start + pageSize
+    total = len(product)
+    num_page = math.ceil(total/pageSize)
+    context = {
+        'product': product[start:end],
+        'start': start,
+        'end': end,
+        'total': total,
+        'num_page': num_page,
+        'page': page
+    }
+    return render(request, 'motor/ktm.html', context)
 
 
 def viewProductDetail(request, pk):
@@ -69,14 +232,14 @@ def orderProduct(request, pk):
         if form.is_valid():
             data = form.cleaned_data
             print('data', data)
-            order=Order()
-            order.product=product
-            order.qty=data['qty']
-            order.customer_name=data['customer_name']
-            order.customer_phone=data['customer_phone']
-            order.customer_address=data['customer_address']
-            order.order_date=datetime.now()
-            order.status=Order.Status.NEW
+            order = Order()
+            order.product = product
+            order.qty = data['qty']
+            order.customer_name = data['customer_name']
+            order.customer_phone = data['customer_phone']
+            order.customer_address = data['customer_address']
+            order.order_date = datetime.now()
+            order.status = Order.Status.NEW
             order.save()
             return redirect('/thank-you')
     context = {'form': form, 'product': product}
