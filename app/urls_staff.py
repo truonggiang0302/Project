9a677944edc0e0b6cdd0b4472c7c7e4727470233
truonggiang0302 <giangkoy0302@gmail.com>
2020-12-01 22:00:06 +0700

    pdate

--- app/urls_staff.py
+++ app/urls_staff.py
@@ -3,4 +3,17 @@ from .views_staff import *
 
 urlpatterns=[
     path('list-category',listCategory,name='list-category'),
+    path('create-category',CategoryCreateView.as_view(),name='create-category'),
+    path('update-category/<pk>',CategoryUpdateView.as_view(),name='update-category'),
+    path('delete-category/<pk>',deleteCategory,name='delete-category'),
+
+    path('list-product',listProduct,name='list-product'),
+    path('create-product',ProductCreateView.as_view(),name='create-product'),
+    path('update-product/<pk>',ProductUpdateView.as_view(),name='update-product'),
+    path('delete-product/<pk>',deleteProduct,name='delete-product'),
+
+    path('list-order',listOrder,name='list-order'),
+    path('view-order/<pk>',viewOrder,name='view-order'),
+    path('confirm-order-deliver/<pk>',confirmOrderDeliver),
+    path('cancel-order/<pk>',cancelOrder),
 ]
--- app/views_staff.py
+++ app/views_staff.py
@@ -8,8 +8,77 @@ from django.conf import settings
 import math
 from django.contrib.auth.decorators import login_required
 
+#Category
 
 def listCategory(request):
     categoryList = Category.objects.all()
     context = {'categoryList': categoryList}
     return render(request, 'staff/category/list.html', context)
+
+class CategoryCreateView(CreateView):
+    model=Category
+    fields='__all__'
+    success_url='/staff/list-category'
+    template_name='staff/category/form.html'
+
+class CategoryUpdateView(UpdateView):
+    model=Category
+    fields='__all__'
+    success_url='/staff/list-category'
+    template_name='staff/category/form.html'
+
+def deleteCategory(request,pk):
+    c=Category.objects.get(pk=pk)
+    c.delete()
+    return redirect('list-category')
+
+#Product
+
+def listProduct(request):
+    productList=Product.objects.all()
+    context={'productList':productList}
+    return render(request,'staff/product/list.html',context)
+
+class ProductCreateView(CreateView):
+    model=Product
+    fields='__all__'
+    success_url='/staff/list-product'
+    template_name='staff/product/form.html'
+
+class ProductUpdateView(UpdateView):
+    model=Product
+    fields='__all__'
+    success_url='/staff/list-product'
+    template_name='staff/product/form.html'
+
+def deleteProduct(request,pk):
+    c=Product.objects.get(pk=pk)
+    c.delete()
+    return redirect('list-product')
+
+#Order
+def listOrder(request):
+    orderList=Order.objects.all()
+    orderList=orderList.order_by('status','-order_date')
+    context={
+        'orderList':orderList
+    }
+    return render(request,'staff/order/list.html',context)
+
+def viewOrder(request,pk):
+    order=Order.objects.get(pk=pk)
+    context={'order':order}
+    return render(request,'staff/order/detail.html',context)
+
+def confirmOrderDeliver(request,pk):
+    order=Order.objects.get(pk=pk)
+    order.status=Order.Status.DELIVERD
+    order.deliver_date=datetime.now()
+    order.save()
+    return redirect('list-order')
+
+def cancelOrder(request,pk):
+    order=Order.objects.get(pk=pk)
+    order.status=Order.Status.CANCELED
+    order.save()
+    return redirect('list-order')
