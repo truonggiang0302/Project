cf7cea3f4e9bb2464bcc01b697f3ff734437d8d8
truonggiang0302 <giangkoy0302@gmail.com>
2020-12-03 15:21:00 +0700

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('sign-up',signUp,name='sign-up'),
+    path('signup', signUp, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
 def signUp(request):
-    return render(request,'registration/signup.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(username=user.username,
+                                password=request.POST['password1'],
+                                email=user.email,
+                                first_name=user.first_name,
+                                last_name=user.last_name)
+            login(request, user)
+            return redirect('/staff')
+
+    return render(request, 'registration/signup.html', {'form':  form})
